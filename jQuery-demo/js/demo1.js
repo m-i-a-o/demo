e6c126434fbbc7af6393daa31b4ff4c499d995ab
m-i-a-o <1610639322@qq.com>
2017-10-31 22:02:46 +0800

    practice

--- jQuery-demo/js/demo1.js
+++ jQuery-demo/js/demo1.js
@@ -1,12 +1,28 @@
-$(function(){//等待DOM加载完毕
-	var $category=$('ul li:gt(5):not(:last)');//获得索引值大于5的品牌集合对象（出最后一条）
-	$category.hide();//隐藏上面获取到的jQuery对象
-	var $toggleBtn=$('div.showmore>a');//获取“显示全部品牌”按钮
-	$toggleBtn.click(function(){//给按钮添加onclick时间
-		if($category.is(":isvisible")){//如果元素显示
-			$category.hide();//隐藏category
-			$(this).find('span')
-			.text("显示全部品牌");
-		}
-	})
-})
+// $(document).ready(function() {
+//          /**
+//           * 通过jQuery语法获取id为imooc2的元素获得一个jQuery对象
+//           * 调用该对象的html()方法进行更改内容
+//           * 调用该对象的css()方法进行更改颜色样式
+//           */   
+//          var $p = $('#imooc2');
+//          $p.html('P2：您好！通过慕课网学习jQuery才是最佳的途径').css('color','red');
+//      });
+//$(document).ready(function(){
+//	var $li=$("ul li:eq(1)");
+//	var li_txt=$li.text();
+//	alert(li_txt);
+//	var $para=$("p");
+//	var p_txt=$para.attr("title");
+//	alert(p_txt);
+//	
+//})
+$(function(){
+	$("#panel").css("opacity","0.5");
+	$("#panel").click(function(){
+		$(this).animate({left:"400px",height:"200px",opacity:"1"},3000)
+		       .animate({top:"200px",width:"200px"},3000, function(){
+		       	$(this).css("border","5px solid blue");
+		       })
+		       
+	});
+});
